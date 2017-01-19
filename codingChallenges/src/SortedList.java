8b0e48bc8f17a8901dd43df6dc38c30ebe4dc452
KirTan <patelkirtann@gmail.com>
2017-01-19 05:38:41 -0800

    Sorted List

---
+++ codingChallenges/src/SortedList.java
@@ -0,0 +1,59 @@
+/**
+ * Created by kt_ki on 1/19/2017.
+ */
+public class SortedList {
+    Main_Link first;
+
+    public SortedList() {
+        first = null;
+    }
+
+    public boolean isEmpty(){
+        return first==null;
+    }
+
+    public void insert(double value) {
+        Main_Link newValue = new Main_Link(value);
+        Main_Link previous = null;
+        Main_Link current = first;
+
+        while (current != null && value > current.data) {
+            previous = current;
+            current = current.next;
+        }
+        if (previous == null) {
+            first = newValue;
+        } else {
+            previous.next = newValue;
+            newValue.next = current;
+        }
+    }
+
+    public double remove() {
+        double temp = first.data;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList(){
+        System.out.print(" List : ");
+        Main_Link current = first;
+        while (current != null){
+            current.displayLink();
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        SortedList sortedList = new SortedList();
+
+        sortedList.insert(23);
+        sortedList.insert(52);
+        sortedList.insert(44);
+
+        sortedList.displayList();
+
+    }
+}
